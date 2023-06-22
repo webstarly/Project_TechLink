ac76ebdec6f4e8745bc33d7dba754198647bc1ba
webstarly <edidionge.ukem@gmail.com>
2023-06-22 23:22:48 +0000

    TechLink

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask,render_template 
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def home():
+    return ("TechLink")
+
+@app.route('/home', strict_slashes=False)
+def homepage():
+    return render_template('homepage.html')
+
+if __name__ == '__main__':
+    app.run()
